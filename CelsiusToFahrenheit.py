0e2525c8729f66776d3790ee02e41164e121d63d
Joseph Varacalli <58127285+joevaracalli@users.noreply.github.com>
2020-02-25 14:07:52 -0500

    Add files via upload

---
+++ CelsiusToFahrenheit.py
@@ -0,0 +1,12 @@
+celsius = 0
+fahrenheit = 0
+celsius1 = float(celsius)
+fahrenheit1 = float(celsius)
+
+
+celsius1 = input ("Enter a temperture: ")
+
+fahrenheit1= (9.0 / 5.0)*(celsius + 32)
+
+print("That temp is equal to")
+print(fahrenheit1,"F")
